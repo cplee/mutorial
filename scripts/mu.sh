dcb6418f789f923af04ff9eaf78402198ea63e72
Dugas, Jeff <jeff.dugas@one.verizon.com>
2017-09-22 16:20:16 -0400

    Remove pathPatterns

--- scripts/mu.sh
+++ scripts/mu.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 curl -s http://getmu.io/install.sh | sh
-sudo apt install -y awscli jq
+sudo apt install python-pip
+pip install --upgrade pip
+pip install awscli --upgrade --user
+sudo apt install -y jq
+
+# aws configure
+# aws ecr get-login --no-include-email
